e28e3a931b73265b044bd4670c107c60c770273c
Jerry Owusu <jerry@ethosdpg.com>
2025-01-13 11:34:47 +0000

    started working on the mobile responsiveness

--- index.js
+++ index.js
@@ -6,9 +6,29 @@
         .catch(error => console.error('Error loading footer:', error));
 
 
-        fetch('header.html')
+    fetch('header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header').innerHTML = data;
         })
         .catch(error => console.error('Error loading footer:', error));
+
+
+        
+        document.addEventListener('DOMContentLoaded', () => {
+            const menuToggle = document.getElementById('menu-toggle');
+            const mobileMenu = document.getElementById('mobile-menu');
+            let menuOpen = false;
+
+            menuToggle.addEventListener('click', () => {
+                menuOpen = !menuOpen;
+
+                if (menuOpen) {
+                    mobileMenu.classList.remove('hidden');
+                    mobileMenu.classList.add('flex');
+                } else {
+                    mobileMenu.classList.add('hidden');
+                    mobileMenu.classList.remove('flex');
+                }
+            });
+        });
